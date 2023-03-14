80249f01c3173bea57bc8c8bb260ecb885a5dffc
Henry Chadban <henryj.chadban@gmail.com>
2023-03-14 13:21:40 +1100

    setup basic website for temp conversion as discussed in the tutorial

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from flask import Flask
+from flask import request,escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Congratulations, it's a web app!"
+    celsius = request.args.get("celsius", "")
+    if celsius:
+        fahrenheit = fahrenheit_from(celsius)
+    else:
+        fahrenheit = ""
+    celsius_input = """<form action="" method="get">
+                    <input type="text" name="celsius">
+                    <input type="submit" value="Convert">
+                    </form>"""
+    return (celsius_input + "Fahrenheit: " + fahrenheit)
+
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    try:
+        fahrenheit = float(celsius) * 9 / 5 + 32
+        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+        return str(fahrenheit)
+    except ValueError:
+        return "invalid input"
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
